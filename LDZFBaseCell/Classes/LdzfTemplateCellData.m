4defe6315aebc7a16c4a8d63b446c213d905af2d
yuhui.Mr <zhuyuhui434@gmail.com>
2022-02-21 11:16:38 +0800

    默认值

--- LDZFBaseCell/Classes/LdzfTemplateCellData.m
+++ LDZFBaseCell/Classes/LdzfTemplateCellData.m
@@ -2,5 +2,12 @@
 #import "LdzfTemplateCellData.h"
 
 @implementation LdzfTemplateCellData
-
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.reuseIdentifier = @"cell";
+    }
+    return self;
+}
 @end
